154c08ff9460a9baafd08553fe1974361ca3d944
Sara Hadzic <saraahadzic@gmail.com>
2020-02-03 09:15:11 +0100

    Changes

--- user-polls/src/main/java/info/interventure/userpolls/model/UserPolls.java
+++ user-polls/src/main/java/info/interventure/userpolls/model/UserPolls.java
@@ -24,7 +24,7 @@ import java.util.Map;
 import java.util.Objects;
 
 
-@Document(collection = "polls", language = "none")
+@Document(collection = "polls", language = "english")
 
 public class UserPolls {
 
